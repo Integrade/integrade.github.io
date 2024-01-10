e9211d0e9ff08d59871f67f60999d46940f9c166
Integrade <esgedco@gmail.com>
2024-01-10 18:09:21 +0700

    all

---
+++ js/coba.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+document.body.appendChild(renderer.domElement);
+
+const scene =  new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(4, 5, 11);
+camera.lookAt(0, 0, 0);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxpolarAngle = 1.5;
+controls.autoRotate = false;
+controls.newTarget = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+    color: 0x555555,
+    side: THREE.DoubleSide
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+scene.add(groundMesh);
+
+const spotLight = new THREE.SpotLight(0xffffff, 2, 50, 0.2, 0.5);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+scene.add(spotLight);
+
+const loader = new GLTFLoader().setPath('./3D/rumah_contoh/')
+loader.load('SampleLevel.glb', (gltf) => {
+    const mesh = gltf.scene;
+
+    mesh.traverse((child) => {
+        if (child.isMesh){
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    })
+
+    mesh.position.set(0, 0, -5);
+    scene.add(mesh);
+})
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
