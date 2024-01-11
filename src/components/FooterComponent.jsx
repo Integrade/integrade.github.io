33e975a4921137cebe48daae6f7092d1dca8b9db
Integrade <esgedco@gmail.com>
2024-01-12 03:33:13 +0700

    tes

--- src/components/FooterComponent.jsx
+++
@@ -1,7 +0,0 @@
-const FooterComponent = () => {
-  return (
-    <div className="navbar-inverse navbar-fixed-bottom">Develop by Integrade Tech</div>
-  )
-}
-
-export default FooterComponent
--- src/pages/Portofolio.jsx
+++
@@ -1,7 +0,0 @@
-const Portofolio = () => {
-  return (
-    <div>Portofolio</div>
-  )
-}
-
-export default Portofolio
