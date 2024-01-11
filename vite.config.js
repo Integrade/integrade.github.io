c2e54f12a1a768dc086284da1097b27ca43ce08a
Integrade <esgedco@gmail.com>
2024-01-12 04:40:22 +0700

    revisi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/integrade.github.io/",
   plugins: [react()],
-  base: "/integrade.github.io/"
 })
